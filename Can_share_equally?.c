f2cd737a543537885cb3d4c68bec3a8f8f698ec1
bikkinahema <102247468+bikkinahema@users.noreply.github.com>
2022-11-05 14:03:46 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int x,y;     cin>>x>>y;     if((x%2==0) and (x>0 or y%2==0)){         cout<<"YES";     }     else cout<<"NO";
    }

---
+++ Can_share_equally?.c
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if((x%2==0) and (x>0 or y%2==0)){
+        cout<<"YES";
+    }
+    else cout<<"NO";
+    
+}
